c25d2e85408f3d08ac1df1da962f477166f42c78
Patrick Freeze <Patfreeze@amedacier.com>
2021-08-21 12:17:33 -0400

    Update for 1.17.1
    - Correction in the language file
    - added more comments
    - added delay to reuse the ForFun command

--- src/com/amedacier/LuckyMan.java
+++ src/com/amedacier/LuckyMan.java
@@ -35,7 +35,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-// Version on spigot 1.0.7
+// Version on spigot 1.0.8
 
 public class LuckyMan extends JavaPlugin implements Listener{
 
@@ -47,7 +47,7 @@ public class LuckyMan extends JavaPlugin implements Listener{
 	String sCorrectColor = "§2"; // Green
 	String sResetColor = "§r";
 	String sPluginName = "§2[LuckyMan] §r";
-	int iVersion = 108; // 107 on spigot
+	int iVersion = 109; // 108 on spigot
 	
 	File getDataFolderLink  = getDataFolder();
 
@@ -118,12 +118,14 @@ public class LuckyMan extends JavaPlugin implements Listener{
     @Override
     public void onEnable(){
 		
+    	// On enable plugin we create configs files
 		try {
 			createFiles();
 		} catch (InvalidConfigurationException e) {
 			e.printStackTrace();
 		}
 		
+		// This is for the auto complete in the chat of the player
 		getCommand("luckyman").setTabCompleter(new LuckyManTabCompleter());
 		
 	 // Enable our class to check for new players using onPlayerJoin()
@@ -132,6 +134,7 @@ public class LuckyMan extends JavaPlugin implements Listener{
 	
 	@Override
 	public void onDisable() {
+		// You can do something here when your plugin was disabled by the owner
 	}
 	
 	// This method checks for incoming players and sends them a message
@@ -142,6 +145,11 @@ public class LuckyMan extends JavaPlugin implements Listener{
 	    player.spawnParticle(Particle.FIREWORKS_SPARK, player.getLocation(), 20);
 	}
 	
+	/**
+	 * This will get the EXP from the player depend of small/medium/high
+	 * @param sType
+	 * @return
+	 */
 	public int getExpLevelCost(String sType) {
 		
 		int iExp = iExpLevelDefault;
@@ -164,10 +172,31 @@ public class LuckyMan extends JavaPlugin implements Listener{
 		return iExp;
 	}
 	
-	public boolean updateRemainTime(Player player) {
+	/**
+	 * Update the remain time after an execution
+	 * @param player
+	 * @param sType
+	 * @return
+	 */
+	public boolean updateRemainTime(Player player, String sType) {
+		
+		// VALIDATE TYPE OF THE REMAINER
+		switch(sType) {
+			case "timeRemain":
+			case "timeRemainForfun":
+				// Nothing to do here we are OK
+				break;
+				
+			default:
+				sendPluginMessage(player, sErrorColor+"Sorry error! Look at console for more detail!");
+				System.out.println(sType+" for the updateRemainTime is not implemented");
+				return false;
+		}
 		
 		//SET CALENDAR
 	    Calendar calendar = Calendar.getInstance();
+	    
+	    // We set the time zone 
 	    calendar.setTimeZone(TimeZone.getTimeZone("America/New_York"));
 	    
 	    // CHECK IF PLAYER IS IN FILE
@@ -177,7 +206,15 @@ public class LuckyMan extends JavaPlugin implements Listener{
 	    	try {
 	    		playerdata.createSection("P_"+player.getUniqueId());
 	    		playerdata.set("P_"+player.getUniqueId()+".name", player.getName());
-	    		playerdata.set("P_"+player.getUniqueId()+".timeRemain", calendar.getTimeInMillis());
+	    		
+	    		// Default value
+	    		playerdata.set("P_"+player.getUniqueId()+".timeRemain", "1009558136984");
+	    		playerdata.set("P_"+player.getUniqueId()+".timeRemainForfun", "1009558136984");
+	    		
+	    		// We update the type currently in use here
+	    		playerdata.set("P_"+player.getUniqueId()+"."+sType, calendar.getTimeInMillis());
+	    		
+	    		// Save data to file
 	    		playerdata.save(playerdataf);
 	    		
 	    		return true;
@@ -187,16 +224,23 @@ public class LuckyMan extends JavaPlugin implements Listener{
 	    }
 	    else {
 	    	// DATA EXIST VERIFY THEN UPDATE IF NEEDED
-	    	iLeft = (calendar.getTimeInMillis() - playerdata.getLong("P_"+player.getUniqueId()+".timeRemain"))/1000;
+	    	if(playerdata.isLong("P_"+player.getUniqueId()+"."+sType)) { // We check if exist
+	    		iLeft = (calendar.getTimeInMillis() - playerdata.getLong("P_"+player.getUniqueId()+"."+sType))/1000;
+	    	}
+	    	else {
+	    		// Is not exist so we need to create this (for old files versions)
+	    		iLeft = config.getLong("iSecondDelay")+50; // We force an update
+	    	}
 	    	
 	    	// CHECK IF WE CAN DO IT AGAIN BY CHECK IN config.yml
 	    	if(iLeft >= config.getLong("iSecondDelay")) {
 	    		// Ex: IF iLeft=70 AND iSecondeDelay=60 HE CAN REUSE IT
 	    		// UPDATE DATA ALSO NAME IF CHANGE IT
 	    		
+	    		// We update some information, we update also the name in case of player have change his/her name
 		    	try {
 		    		playerdata.set("P_"+player.getUniqueId()+".name", player.getName());
-		    		playerdata.set("P_"+player.getUniqueId()+".timeRemain", calendar.getTimeInMillis());
+		    		playerdata.set("P_"+player.getUniqueId()+"."+sType, calendar.getTimeInMillis());
 		    		playerdata.save(playerdataf);
 		    	} catch (IOException exception) {
 		    		exception.printStackTrace();
@@ -213,6 +257,11 @@ public class LuckyMan extends JavaPlugin implements Listener{
 	    return false;
 	}
 
+	/**
+	 * This will play a litle music (nothing fancy)
+	 * @param player
+	 * @return
+	 */
 	private int playMusic(Player player) {
 		
 		int iTime = 0;
@@ -222,6 +271,13 @@ public class LuckyMan extends JavaPlugin implements Listener{
 		return iTime;
 	}
 	
+	/**
+	 * This will return a sound
+	 * @param player
+	 * @param dTest
+	 * @param iTick
+	 * @return
+	 */
 	private int playSound(Player player, Float dTest, int iTick) {
 		
 		Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
@@ -237,11 +293,15 @@ public class LuckyMan extends JavaPlugin implements Listener{
 		return iTick;
 	}
 	
+	/**
+	 * This return the errorSound
+	 * @param p
+	 */
 	private void playErrorSound(Player p) {
 		playSound(p, 4F, 2);
 		playSound(p, 1F, 4);
 	}
-	
+
 	@Override
 	public boolean onCommand(CommandSender sender,
 		Command command,
@@ -264,6 +324,7 @@ public class LuckyMan extends JavaPlugin implements Listener{
 			}
 		}
 		
+		// If this is not in the good world advise the player
 		if(!bInGoodWorld) {
 			sendPluginMessage(player1, sErrorColor+" disabled for this world");
 			return true;
@@ -282,8 +343,15 @@ public class LuckyMan extends JavaPlugin implements Listener{
 		}
 		else if (command.getName().equalsIgnoreCase("luckyman") && args[0].equalsIgnoreCase("forfun")) {
 			
+			// UPDATE TIME AFTER ITEM AND CHECK IF OK (added to not abuse/spam it)
+			if(!updateRemainTime(player1,"timeRemainForfun")) {
+				playErrorSound(player1);
+				return true;
+			}
+			
 			int iStep = 0;
 			
+			// Take a random number between 0 to 5 
 		    Random randomGenerator = new Random();
 		    for (int idx = 0; idx <= 10; ++idx){
 		    	iStep = randomGenerator.nextInt(6);
@@ -292,6 +360,7 @@ public class LuckyMan extends JavaPlugin implements Listener{
 			// COMMAND FOR FUN HERE
 		    PotionEffect Potion;
 			switch(iStep) {
+			
 			case 0:
 				Potion = new PotionEffect(PotionEffectType.CONFUSION, 200, 2);
 				player1.playSound(player1.getLocation(),Sound.ENTITY_ZOMBIE_AMBIENT, 1F, 1F);
@@ -332,7 +401,7 @@ public class LuckyMan extends JavaPlugin implements Listener{
 				player1.performCommand("me "+language.getString("BeenTroll"));
 				break;
 			
-			default:
+			default: // default if we hit 5 or anything else if we change the 6 of anything else more than that
 				player1.playSound(player1.getLocation(),Sound.ENTITY_DRAGON_FIREBALL_EXPLODE, 1F, 1F);
 				player1.playSound(player1.getLocation(),Sound.ENTITY_DRAGON_FIREBALL_EXPLODE, 2F, 2F);
 				sendPluginMessage(player1,sObjectColor+language.getString("ArgExplode"));
@@ -343,34 +412,31 @@ public class LuckyMan extends JavaPlugin implements Listener{
 			return true;
 		}
 		else if (command.getName().equalsIgnoreCase("luckyman") && args[0].equalsIgnoreCase("getluck")) {
-		
-			//DEFAULT TYPE IS SMALL
-			String sType = "small";
-			
+
+			// If not arg after getluck
 			if(args.length == 1) {
 				sendPluginMessage(player1,sObjectColor+String.format(language.getString("helpType"), "small, medium, high"));
 				return true;
 			}
 			
+			String sType = "small";
 			if(args.length > 1) {
-				if(args[1].equalsIgnoreCase("medium")) {
-					sType = "medium";
-					this.sTypeChoosen = sType;
-				}
-				else if(args[1].equalsIgnoreCase("small")) {
-					sType = "small";
-					this.sTypeChoosen = sType;
-				}
-				else if(args[1].equalsIgnoreCase("high")) {
-					sType = "high";
-					this.sTypeChoosen = sType;
-				}
-				else {
-					sendPluginMessage(player1,sObjectColor+String.format(language.getString("getLuckType"),args[1]));
-					return true;
+				// switch between types
+				sType = args[1].toLowerCase();
+				switch(sType) {
+					case "small":
+					case "medium":
+					case "high":
+						this.sTypeChoosen = sType;
+						break;
+					
+					default:
+						sendPluginMessage(player1,sObjectColor+String.format(language.getString("getLuckType"),args[1]));
+						return true;
 				}
 			}
 			
+			// If it from the console
 			if (!(sender instanceof Player)) {
 				sender.sendMessage(sPluginName+sErrorColor+"This command can only be run by a player.");
 			} else {
@@ -381,13 +447,14 @@ public class LuckyMan extends JavaPlugin implements Listener{
 					sendPluginMessage(player1,sObjectColor+language.getString("NothingHand"));
 				}
 				else if(config.getBoolean("UseExpCost") && player1.getLevel() < getExpLevelCost(sType)) {
+					// If the player have not the level we need
 					playErrorSound(player1);
 					sendPluginMessage(player1,sObjectColor+String.format(language.getString("NeedXP"), getExpLevelCost(sType)));
 				}
 				else {
 					
 					// UPDATE TIME AFTER ITEM AND CHECK IF OK
-					if(!updateRemainTime(player1)) {
+					if(!updateRemainTime(player1, "timeRemain")) {
 						playErrorSound(player1);
 						return true;
 					}
@@ -479,6 +546,11 @@ public class LuckyMan extends JavaPlugin implements Listener{
 	    }
 	}
 	
+	/**
+	 * This return and object Stack
+	 * @param sStackName
+	 * @return
+	 */
 	public ItemStack getItemStackByName(String sStackName) {
 		ItemStack objStack = null;
 		if(Material.getMaterial(sStackName) != null) {
@@ -495,6 +567,12 @@ public class LuckyMan extends JavaPlugin implements Listener{
 		return objStack;
 	}
 	
+	/**
+	 * This will exchange the old one with a new item
+	 * @param ItemStackOld
+	 * @param sender
+	 * @return
+	 */
 	public ItemStack getChangeItemStack(ItemStack ItemStackOld, CommandSender sender) {
 		
 			// More than one object just throw all except one
@@ -503,6 +581,7 @@ public class LuckyMan extends JavaPlugin implements Listener{
 				ItemStack ItemStackThrow = new ItemStack(ItemStackOld.getType());
 				ItemStackThrow.setAmount(ItemStackOld.getAmount()-1);
 				
+				// If player have, for example 5 apples we throw in the air 4 of them and exchange only one apple
 				Player player = (Player) sender;
 				ItemStackOld.setAmount(1);
 				player.getWorld().dropItem(player.getLocation().add(0, 5, 0), ItemStackThrow);
@@ -601,10 +680,12 @@ public class LuckyMan extends JavaPlugin implements Listener{
 				}
 			}
 			else {
+				// Bad error here we cancel the transaction and return the old stack!
 				sendPluginMessage((Player) sender,sErrorColor+language.getString("Oupps"));
 				return ItemStackOld;
 			}
 			
+			// Successful we play the little music
 			playMusic((Player) sender);
 			return objStack;
 	}
